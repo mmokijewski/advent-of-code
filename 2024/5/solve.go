602ee183891658b59b345a5a7f661cc000607925
mmokijewski <mareczek8@tlen.pl>
2024-12-05 10:28:19 +0100

    day 5 part 2

--- 2024/5/solve.go
+++ 2024/5/solve.go
@@ -10,11 +10,49 @@ import (
 	"time"
 )
 
+func checkSingeRule(update string, rule string) bool {
+	leftNum := strings.Split(rule, "|")[0]
+	rightNum := strings.Split(rule, "|")[1]
+	reg := regexp.MustCompile(rightNum + ",.*" + leftNum)
+	result := reg.FindString(update)
+	if len(result) != 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func checkIfCorrect(update string, rules []string) bool {
+	i := 0
+	for i < len(rules) {
+		if !checkSingeRule(update, rules[i]) {
+			return false
+		}
+		i++
+	}
+	return true
+}
+
+func replaceNumbers(update string, rule string) string {
+	leftNum := strings.Split(rule, "|")[0]
+	rightNum := strings.Split(rule, "|")[1]
+	update = strings.Replace(update, leftNum, rightNum, 1)
+	update = strings.Replace(update, rightNum, leftNum, 1)
+	return update
+}
+
+func getMiddleNum(update string) int {
+	numbers := strings.Split(update, ",")
+	middleNum, _ := strconv.Atoi(numbers[len(numbers)/2])
+	return middleNum
+}
+
 func main() {
 	start := time.Now()
 	inputFile, _ := os.Open("input")
 
 	part1Sum := 0
+	part2Sum := 0
 	var rules []string
 	var updates []string
 
@@ -30,31 +68,24 @@ func main() {
 		}
 	}
 
-	rulesCount := len(rules)
-
 	for i := range updates {
-
-		isCorrect := true
-		j := 0
-
-		for isCorrect && j < rulesCount {
-			leftNum := strings.Split(rules[j], "|")[0]
-			rightNum := strings.Split(rules[j], "|")[1]
-			reg := regexp.MustCompile(rightNum + ",.*" + leftNum)
-			result := reg.FindString(updates[i])
-			if len(result) != 0 {
-				isCorrect = false
-			}
-			j++
-		}
-
+		isCorrect := checkIfCorrect(updates[i], rules)
 		if isCorrect {
-			numbers := strings.Split(updates[i], ",")
-			middleNum, _ := strconv.Atoi(numbers[len(numbers)/2])
-			part1Sum += middleNum
+			part1Sum += getMiddleNum(updates[i])
+		} else {
+			for !isCorrect {
+				for j := range rules {
+					if !checkSingeRule(updates[i], rules[j]) {
+						updates[i] = replaceNumbers(updates[i], rules[j])
+					}
+				}
+				isCorrect = checkIfCorrect(updates[i], rules)
+			}
+			part2Sum += getMiddleNum(updates[i])
 		}
 	}
 
 	fmt.Printf("Part 1 sum: %d\n", part1Sum)
+	fmt.Printf("Part 2 sum: %d\n", part2Sum)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
