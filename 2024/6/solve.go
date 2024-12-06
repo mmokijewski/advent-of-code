7cd972cefc5bd2426806d557651268541c30f5c3
mmokijewski <mareczek8@tlen.pl>
2024-12-07 00:19:55 +0100

    day 6 part 2

--- 2024/6/solve.go
+++ 2024/6/solve.go
@@ -8,15 +8,15 @@ import (
 	"time"
 )
 
-func goFurther(playground []string, posY int, posX int, direction int) []int {
+func goFurther(playground [][]string, posY int, posX int, direction int) []int {
 	switch direction {
 	case 1: //^
 		if posY == 0 {
 			return []int{-1, -1, -1}
 		}
-		next := string(playground[posY-1][posX])
+		next := playground[posY-1][posX]
 		if next == "#" {
-			return []int{posY, posX + 1, 3}
+			return []int{posY, posX, 3}
 		} else {
 			return []int{posY - 1, posX, 1}
 		}
@@ -24,9 +24,9 @@ func goFurther(playground []string, posY int, posX int, direction int) []int {
 		if posY == len(playground)-1 {
 			return []int{-1, -1, -1}
 		}
-		next := string(playground[posY+1][posX])
+		next := playground[posY+1][posX]
 		if next == "#" {
-			return []int{posY, posX - 1, 4}
+			return []int{posY, posX, 4}
 		} else {
 			return []int{posY + 1, posX, 2}
 		}
@@ -34,9 +34,9 @@ func goFurther(playground []string, posY int, posX int, direction int) []int {
 		if posX == len(playground[1])-1 {
 			return []int{-1, -1, -1}
 		}
-		next := string(playground[posY][posX+1])
+		next := playground[posY][posX+1]
 		if next == "#" {
-			return []int{posY + 1, posX, 2}
+			return []int{posY, posX, 2}
 		} else {
 			return []int{posY, posX + 1, 3}
 		}
@@ -44,9 +44,9 @@ func goFurther(playground []string, posY int, posX int, direction int) []int {
 		if posX == 0 {
 			return []int{-1, -1, -1}
 		}
-		next := string(playground[posY][posX-1])
+		next := playground[posY][posX-1]
 		if next == "#" {
-			return []int{posY - 1, posX, 1}
+			return []int{posY, posX, 1}
 		} else {
 			return []int{posY, posX - 1, 4}
 		}
@@ -54,25 +54,55 @@ func goFurther(playground []string, posY int, posX int, direction int) []int {
 	return []int{-1, -1, -1}
 }
 
+func checkIfLoop(playground [][]string, posY int, posX int, direction int) bool {
+
+	var visitedPositionsWithDir []string
+	tempPosY := posY
+	tempPosX := posX
+	tempDir := direction
+
+	for tempDir != -1 {
+		stringPositionWithDir := fmt.Sprintf("%d,%d,%d", tempPosY, tempPosX, tempDir)
+		if slices.Contains(visitedPositionsWithDir, stringPositionWithDir) {
+			return true
+		}
+
+		visitedPositionsWithDir = append(visitedPositionsWithDir, stringPositionWithDir)
+		move := goFurther(playground, tempPosY, tempPosX, tempDir)
+
+		tempPosY = move[0]
+		tempPosX = move[1]
+		tempDir = move[2]
+	}
+	return false
+}
+
 func main() {
 	start := time.Now()
 	inputFile, _ := os.Open("input")
 
-	var playground []string
+	var startPositionY int
+	var startPositionX int
+	var playground [][]string
 	var direction int
 	var posX int
 	var posY int
 	var visitedPositions []string
+	var newHashesProposal []string
 
 	i := 0
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
 		line := scanner.Text()
-		playground = append(playground, line)
+		row := make([]string, 0)
 		for j, sign := range line {
+			char := string(sign)
+			row = append(row, char)
 			if string(sign) != "." && string(sign) != "#" {
 				posX = j
 				posY = i
+				startPositionX = j
+				startPositionY = i
 				strDirection := string(sign)
 				if strDirection == "^" {
 					direction = 1
@@ -85,6 +115,7 @@ func main() {
 				}
 			}
 		}
+		playground = append(playground, row)
 		i++
 	}
 	for direction != -1 {
@@ -92,13 +123,23 @@ func main() {
 		if !slices.Contains(visitedPositions, stringPosition) {
 			visitedPositions = append(visitedPositions, stringPosition)
 		}
+
+		if playground[posY][posX] == "." {
+			playground[posY][posX] = "#"
+			tempHashPos := fmt.Sprintf("%d,%d", posY, posX)
+			if !slices.Contains(newHashesProposal, tempHashPos) && checkIfLoop(playground, startPositionY, startPositionX, 1) {
+				newHashesProposal = append(newHashesProposal, tempHashPos)
+			}
+			playground[posY][posX] = "."
+		}
+
 		move := goFurther(playground, posY, posX, direction)
 		posY = move[0]
 		posX = move[1]
 		direction = move[2]
-
 	}
 
 	fmt.Printf("Part 1 sum: %d\n", len(visitedPositions))
+	fmt.Printf("Part 2 sum: %d\n", len(newHashesProposal))
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
