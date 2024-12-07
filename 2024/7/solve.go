18b2e897a7fd151d1dd31de4b0171824ad69a525
mmokijewski <mareczek8@tlen.pl>
2024-12-07 11:21:03 +0100

    day 7 part 2

--- 2024/7/solve.go
+++ 2024/7/solve.go
@@ -9,22 +9,33 @@ import (
 	"time"
 )
 
-func check(expectedResult int, numbers []int, currentIndex int, currentResult int) bool {
+func check(expectedResult int, numbers []int, currentIndex int, currentResult int, part2 bool) bool {
 	if currentIndex == len(numbers) {
 		return currentResult == expectedResult
 	}
 
 	tempResult := currentResult + numbers[currentIndex]
 	if tempResult <= expectedResult {
-		if check(expectedResult, numbers, currentIndex+1, tempResult) {
+		if check(expectedResult, numbers, currentIndex+1, tempResult, part2) {
 			return true
 		}
 	}
 
 	tempResult = currentResult * numbers[currentIndex]
 	if tempResult <= expectedResult {
-		if check(expectedResult, numbers, currentIndex+1, tempResult) {
+		if check(expectedResult, numbers, currentIndex+1, tempResult, part2) {
 			return true
+		}
+	}
+
+	if part2 {
+		combinedNumString := fmt.Sprintf("%d%d", currentResult, numbers[currentIndex])
+		combinedResult, _ := strconv.Atoi(combinedNumString)
+
+		if combinedResult <= expectedResult {
+			if check(expectedResult, numbers, currentIndex+1, combinedResult, part2) {
+				return true
+			}
 		}
 	}
 	return false
@@ -35,6 +46,7 @@ func main() {
 	inputFile, _ := os.Open("input")
 
 	var part1Sum int
+	var part2Sum int
 
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
@@ -46,12 +58,16 @@ func main() {
 			number, _ := strconv.Atoi(num)
 			numbers = append(numbers, number)
 		}
-		if check(expectedResult, numbers, 0, 0) {
+		if check(expectedResult, numbers, 0, 0, false) {
 			part1Sum += expectedResult
+		}
+		if check(expectedResult, numbers, 0, 0, true) {
+			part2Sum += expectedResult
 		}
 
 	}
 
 	fmt.Printf("Part 1 sum: %d\n", part1Sum)
+	fmt.Printf("Part 1 sum: %d\n", part2Sum)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
