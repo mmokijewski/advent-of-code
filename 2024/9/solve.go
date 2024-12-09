6b6205d7c6de9bc51d819be871e9a7526262acae
mmokijewski <mareczek8@tlen.pl>
2024-12-09 09:47:39 +0100

    day 9 part 2

--- 2024/9/solve.go
+++ 2024/9/solve.go
@@ -10,39 +10,115 @@ import (
 	"time"
 )
 
-func part1(line string) int {
-	var numLine []int
+func part1(numLine []int) int {
+	var filteredLine []int
 	sum := 0
-	for i, sign := range strings.Split(line, "") {
-		currentNum, _ := strconv.Atoi(sign)
+	for i, num := range numLine {
 		space := i%2 == 1
-		for j := 0; j < currentNum; j++ {
+		for j := 0; j < num; j++ {
 			if space {
-				numLine = append(numLine, -1)
+				filteredLine = append(filteredLine, -1)
 			} else {
-				numLine = append(numLine, i/2)
+				filteredLine = append(filteredLine, i/2)
 			}
 		}
 	}
 
-	arrayLen := len(numLine)
+	arrayLen := len(filteredLine)
 	numLineReverted := make([]int, arrayLen)
-	copy(numLineReverted, numLine)
+	copy(numLineReverted, filteredLine)
 	slices.Reverse(numLineReverted)
 	for i, num := range numLineReverted {
-		firstSpace := slices.Index(numLine, -1)
+		firstSpace := slices.Index(filteredLine, -1)
 		if firstSpace == -1 || firstSpace > arrayLen-i {
 			break
 		}
 		if num == -1 {
 			continue
 		} else {
-			numLine[firstSpace] = num
-			numLine = slices.Delete(numLine, arrayLen-1-i, arrayLen-i)
+			filteredLine[firstSpace] = num
+			filteredLine = slices.Delete(filteredLine, arrayLen-1-i, arrayLen-i)
 		}
 	}
 
+	for i, num := range filteredLine {
+		if num != -1 {
+			sum += i * num
+		}
+	}
+
+	return sum
+}
+
+func part2(numLine []int) int {
+
+	sum := 0
+	var lineInBlocks [][]int
+	var finalArray []int
+
 	for i, num := range numLine {
+		space := i%2 == 1
+		var tempBlock []int
+		for j := 0; j < num; j++ {
+			if space {
+				tempBlock = append(tempBlock, -1)
+			} else {
+				tempBlock = append(tempBlock, i/2)
+			}
+		}
+		lineInBlocks = append(lineInBlocks, tempBlock)
+	}
+
+	blockArrayLen := len(lineInBlocks)
+	for i := 0; i < blockArrayLen; i++ {
+		block := lineInBlocks[i]
+		if slices.Contains(block, -1) {
+			currentBlock := block
+			for {
+				blockLength := len(currentBlock)
+				if blockLength == 0 {
+					break
+				}
+				findBlockToFillWith := func(E []int) bool {
+					if len(E) > 0 && len(E) <= blockLength && !slices.Contains(E, -1) {
+						return true
+					} else {
+						return false
+					}
+				}
+				slices.Reverse(lineInBlocks)
+				blockToFillWithIndex := slices.IndexFunc(lineInBlocks, findBlockToFillWith)
+				slices.Reverse(lineInBlocks)
+				normalIndex := blockArrayLen - 1 - blockToFillWithIndex
+				if blockToFillWithIndex == -1 || normalIndex < i {
+					finalArray = append(finalArray, currentBlock...)
+					break
+				} else {
+					desiredBlock := lineInBlocks[normalIndex]
+					finalArray = append(finalArray, desiredBlock...)
+					var newEmptyBlock []int
+					for j := 0; j < len(desiredBlock); j++ {
+						newEmptyBlock = append(newEmptyBlock, -1)
+					}
+					lineInBlocks[normalIndex] = newEmptyBlock
+					lenDiff := blockLength - len(desiredBlock)
+					if lenDiff > 0 {
+						var dummyBlock []int
+						for j := 0; j < lenDiff; j++ {
+							dummyBlock = append(dummyBlock, -1)
+						}
+						currentBlock = dummyBlock
+					} else {
+						currentBlock = []int{}
+					}
+				}
+			}
+		} else {
+			finalArray = append(finalArray, block...)
+		}
+	}
+
+	for i, num := range finalArray {
 		if num != -1 {
 			sum += i * num
 		}
@@ -55,15 +131,21 @@ func main() {
 	start := time.Now()
 	inputFile, _ := os.Open("input")
 
-	var line string
+	var numLine []int
 
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
-		line = scanner.Text()
+		line := scanner.Text()
+		for _, sign := range strings.Split(line, "") {
+			currentNum, _ := strconv.Atoi(sign)
+			numLine = append(numLine, currentNum)
+		}
 	}
 
-	part1Sum := part1(line)
+	part1Sum := part1(numLine)
+	part2Sum := part2(numLine)
 
 	fmt.Printf("Part 1 sum: %d\n", part1Sum)
+	fmt.Printf("Part 2 sum: %d\n", part2Sum)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
