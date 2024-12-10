581c22eb56b633f85b62c795e3482ce2f5bf6483
mmokijewski <mareczek8@tlen.pl>
2024-12-10 08:11:43 +0100

    day 10 part 2

--- 2024/10/solve.go
+++ 2024/10/solve.go
@@ -39,6 +39,28 @@ func getScore(board [][]int, point [2]int, expectedHeight int) map[[2]int]bool {
 	return summits
 }
 
+func getScorePart2(board [][]int, point [2]int, expectedHeight int) int {
+	trails := 0
+	posY := point[0]
+	posX := point[1]
+	if posY < 0 || posY >= len(board) || posX < 0 || posX >= len(board[0]) {
+		return trails
+	}
+
+	height := board[posY][posX]
+	if height != expectedHeight {
+		return trails
+	} else if height == 9 {
+		trails++
+	} else {
+		trails += getScorePart2(board, [2]int{posY - 1, posX}, height+1)
+		trails += getScorePart2(board, [2]int{posY + 1, posX}, height+1)
+		trails += getScorePart2(board, [2]int{posY, posX + 1}, height+1)
+		trails += getScorePart2(board, [2]int{posY, posX - 1}, height+1)
+	}
+	return trails
+}
+
 func main() {
 	start := time.Now()
 	inputFile, _ := os.Open("input")
@@ -46,6 +68,7 @@ func main() {
 	var board [][]int
 	var trailheads [][2]int
 	part1Sum := 0
+	part2Sum := 0
 
 	scanner := bufio.NewScanner(inputFile)
 	i := 0
@@ -65,8 +88,10 @@ func main() {
 
 	for _, trailhead := range trailheads {
 		part1Sum += len(getScore(board, trailhead, 0))
+		part2Sum += getScorePart2(board, trailhead, 0)
 	}
 
 	fmt.Printf("Part 1 sum: %d\n", part1Sum)
+	fmt.Printf("Part 2 sum: %d\n", part2Sum)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
