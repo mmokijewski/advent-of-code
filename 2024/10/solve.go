cf0a7657c0943aa7f9bceeba54dedc61a4b1cd9f
mmokijewski <mareczek8@tlen.pl>
2024-12-10 10:48:42 +0100

    day 10 refactor

--- 2024/10/solve.go
+++ 2024/10/solve.go
@@ -9,58 +9,36 @@ import (
 	"time"
 )
 
-func directions() map[string][2]int {
+func getScore(board [][]int, point [2]int, expectedHeight int) (summitsPart1 map[[2]int]bool, trailsPart2 int) {
+	summitsPart1 = make(map[[2]int]bool)
+	trailsPart2 = 0
+	posY := point[0]
+	posX := point[1]
+	if posY < 0 || posY >= len(board) || posX < 0 || posX >= len(board[0]) {
+		return
+	}
+
 	directions := map[string][2]int{
 		"up":    {-1, 0},
 		"down":  {1, 0},
 		"left":  {0, -1},
 		"right": {0, 1},
 	}
-	return directions
-}
-
-func getScore(board [][]int, point [2]int, expectedHeight int) map[[2]int]bool {
-	summits := make(map[[2]int]bool)
-	posY := point[0]
-	posX := point[1]
-	if posY < 0 || posY >= len(board) || posX < 0 || posX >= len(board[0]) {
-		return summits
-	}
 
 	height := board[posY][posX]
-	if height != expectedHeight {
-		return summits
-	} else if height == 9 {
-		summits[point] = true
-	} else {
-		for _, dir := range directions() {
-			for summit := range getScore(board, [2]int{posY + dir[0], posX + dir[1]}, height+1) {
-				summits[summit] = true
+	if height == expectedHeight && height == 9 {
+		summitsPart1[point] = true
+		trailsPart2++
+	} else if height == expectedHeight {
+		for _, dir := range directions {
+			summits, trails := getScore(board, [2]int{posY + dir[0], posX + dir[1]}, height+1)
+			for summit := range summits {
+				summitsPart1[summit] = true
 			}
+			trailsPart2 += trails
 		}
 	}
-	return summits
-}
-
-func getScorePart2(board [][]int, point [2]int, expectedHeight int) int {
-	trails := 0
-	posY := point[0]
-	posX := point[1]
-	if posY < 0 || posY >= len(board) || posX < 0 || posX >= len(board[0]) {
-		return trails
-	}
-
-	height := board[posY][posX]
-	if height != expectedHeight {
-		return trails
-	} else if height == 9 {
-		trails++
-	} else {
-		for _, dir := range directions() {
-			trails += getScorePart2(board, [2]int{posY + dir[0], posX + dir[1]}, height+1)
-		}
-	}
-	return trails
+	return
 }
 
 func main() {
@@ -89,8 +67,9 @@ func main() {
 	}
 
 	for _, trailhead := range trailheads {
-		part1Sum += len(getScore(board, trailhead, 0))
-		part2Sum += getScorePart2(board, trailhead, 0)
+		summits, trails := getScore(board, trailhead, 0)
+		part1Sum += len(summits)
+		part2Sum += trails
 	}
 
 	fmt.Printf("Part 1 sum: %d\n", part1Sum)
