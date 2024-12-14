065e500df66268442d77fb2bfdadef1063429579
mmokijewski <mareczek8@tlen.pl>
2024-12-14 16:13:28 +0100

    day 12 part 2

--- 2024/12/solve.go
+++ 2024/12/solve.go
@@ -8,21 +8,22 @@ import (
 	"time"
 )
 
-func flood(garden [][]string, point [2]int, visitedFields map[[2]int]bool) int {
+func flood(garden [][]string, point [2]int, visitedFields map[[2]int]bool) (int, int) {
 	area := 0
 	score := 0
 	newRegion := [][2]int{point}
 	visitedFields[point] = true
+	edges := make(map[[3]int]bool)
 
 	gardenHeight := len(garden)
 	gardenWidth := len(garden[0])
 	plantType := garden[point[0]][point[1]]
 
-	directions := map[string][2]int{
-		"up":    {-1, 0},
-		"down":  {1, 0},
-		"left":  {0, -1},
-		"right": {0, 1},
+	directions := map[int][3]int{
+		0: {-1, 0}, //up
+		1: {1, 0},  //down
+		2: {0, -1}, //left
+		3: {0, 1},  //right
 	}
 
 	for len(newRegion) > 0 {
@@ -30,16 +31,18 @@ func flood(garden [][]string, point [2]int, visitedFields map[[2]int]bool) int {
 		newRegion = newRegion[1:]
 		area++
 
-		for _, dir := range directions {
+		for dirIndex, dir := range directions {
 			y := currentField[0] + dir[0]
 			x := currentField[1] + dir[1]
 			newPos := [2]int{y, x}
 			if y < 0 || y >= gardenHeight || x < 0 || x >= gardenWidth {
 				score++
+				edges[[3]int{currentField[0], currentField[1], dirIndex}] = true
 			} else {
 				nearFieldPlantType := garden[y][x]
 				if nearFieldPlantType != plantType {
 					score++
+					edges[[3]int{currentField[0], currentField[1], dirIndex}] = true
 				} else if !visitedFields[newPos] {
 					visitedFields[newPos] = true
 					newRegion = append(newRegion, newPos)
@@ -47,8 +50,29 @@ func flood(garden [][]string, point [2]int, visitedFields map[[2]int]bool) int {
 			}
 		}
 	}
-	fmt.Printf("Plant: %s, area: %d, score: %d\n", plantType, area, score)
-	return area * score
+	for edge := range edges {
+		dir := edge[2]
+		if dir == 0 || dir == 1 { //horizontal edge
+			_, rightExists := edges[[3]int{edge[0], edge[1] + 1, dir}]
+			if rightExists {
+				edges[[3]int{edge[0], edge[1] + 1, dir}] = false
+			}
+		} else { //vertical edge
+			_, downExists := edges[[3]int{edge[0] - 1, edge[1], dir}]
+			if downExists {
+				edges[[3]int{edge[0] - 1, edge[1], dir}] = false
+			}
+		}
+	}
+
+	edgesCount := 0
+	for _, edge := range edges {
+		if edge == true {
+			edgesCount++
+		}
+	}
+
+	return area * score, area * edgesCount
 }
 
 func main() {
@@ -69,15 +93,19 @@ func main() {
 	}
 
 	part1Sum := 0
+	part2Sum := 0
 	for y, line := range garden {
 		for x := range line {
 			point := [2]int{y, x}
 			if !visitedFields[point] {
-				part1Sum += flood(garden, point, visitedFields)
+				part1, part2 := flood(garden, point, visitedFields)
+				part1Sum += part1
+				part2Sum += part2
 			}
 		}
 	}
 
 	fmt.Printf("Part 1: %d\n", part1Sum)
+	fmt.Printf("Part 2: %d\n", part2Sum)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
