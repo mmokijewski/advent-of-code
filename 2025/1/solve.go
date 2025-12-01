edc1cb909c00f3fc5747d3bd6eda2d03a178976f
mmokijewski <marek.mokijewski@gmail.com>
2025-12-01 19:43:56 +0100

    2025 day 1 part 2

--- 2025/1/solve.go
+++ 2025/1/solve.go
@@ -20,26 +20,52 @@ func main() {
 	checkError(err)
 
 	pos := 50
-	count := 0
+	countPart1 := 0
+	countPart2 := 0
+	var prevPos int
 
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
 		line := scanner.Text()
 		dir := line[0:1]
 		dist, _ := strconv.Atoi(line[1:])
+		prevPos = pos
 		if dir == "R" {
-			pos = (pos + dist) % 100
+			divideResult := (pos + dist) / 100
+			divideRest := (pos + dist) % 100
+			if divideResult > 0 {
+				// It's how many times it went through 0 or ended on 0 when turning to right
+				countPart2 += divideResult
+			}
+			pos = divideRest
 		} else {
-			pos = (pos - dist) % 100
+			divideResult := (pos - dist) / -100
+			divideRest := (pos - dist) % 100
+			if divideResult > 0 {
+				// It how many times it went through 0 when turning to left
+				countPart2 += divideResult
+			}
+			// Additional case when it did not go through 0 but just ended on 0 when turning to left
+			if divideRest == 0 {
+				countPart2++
+			}
+
+			pos = divideRest
 			if pos < 0 {
+				// Case when it went through the 0 just once when turning to left
+				if prevPos != 0 {
+					countPart2++
+				}
 				pos = 100 + pos
 			}
 		}
 
 		if pos == 0 {
-			count++
+			countPart1++
 		}
 	}
-	fmt.Printf("Count: %d\n", count)
+
+	fmt.Printf("Count: %d\n", countPart1)
+	fmt.Printf("Count part2: %d\n", countPart2)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
