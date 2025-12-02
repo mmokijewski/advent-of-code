ff0ff4c8f43f42f48fd3c24aff3a02aa1f49d878
mmokijewski <marek.mokijewski@gmail.com>
2025-12-02 09:59:00 +0100

    2025 day 1 part 2

--- 2025/2/solve.go
+++ 2025/2/solve.go
@@ -15,12 +15,30 @@ func checkError(err error) {
 	}
 }
 
+func checkId(idString string, repeats int) bool {
+	idLength := len(idString)
+	for i := 0; i < idLength/repeats; i++ {
+		digits := strings.Split(idString, "")
+
+		for j := range repeats {
+			if digits[i] != digits[i+(j*idLength/repeats)] {
+				return false
+			}
+		}
+		if i == (idLength/repeats)-1 {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	start := time.Now()
 	inputFile, err := os.Open("input")
 	checkError(err)
 
 	part1sum := 0
+	part2sum := 0
 
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
@@ -28,20 +46,20 @@ func main() {
 		for _, idRange := range strings.Split(line, ",") {
 			startId, _ := strconv.Atoi(strings.Split(idRange, "-")[0])
 			endId, _ := strconv.Atoi(strings.Split(idRange, "-")[1])
-			fmt.Println(startId, endId)
 			for i := startId; i <= endId; i++ {
 				idString := strconv.Itoa(i)
 				idLength := len(idString)
-				if idLength%2 != 0 {
-					continue
-				}
-				for j := 0; j < idLength/2; j++ {
-					digits := strings.Split(idString, "")
-					if digits[j] != digits[j+(idLength/2)] {
-						break
-					}
-					if j == idLength/2-1 {
-						part1sum += i
+				for repeats := 2; repeats <= idLength; repeats++ {
+					if idLength%repeats == 0 {
+						if checkId(idString, repeats) {
+							if repeats == 2 {
+								part1sum += i
+								part2sum += i
+							} else {
+								part2sum += i
+							}
+							break
+						}
 					}
 				}
 			}
@@ -49,5 +67,6 @@ func main() {
 	}
 
 	fmt.Printf("Part 1 sum: %d\n", part1sum)
+	fmt.Printf("Part 2 sum: %d\n", part2sum)
 	fmt.Printf("Total time elapsed: %dms\n", time.Since(start).Milliseconds())
 }
